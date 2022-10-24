23d494108ec9813e68e9786f85828acff66c04e7
Charlton Rosero <charlton.r@ravebizz.com>
2022-10-24 16:50:16 -0400

    Implemented Compose Destinations Library.

--- app/src/main/java/com/example/twitty/MainActivity.kt
+++ app/src/main/java/com/example/twitty/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.twitty
 
+import android.graphics.Path.Direction
 import android.os.Bundle
 import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -19,12 +23,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.twitty.destinations.HomeScreenDestination
 import com.example.twitty.ui.theme.TwittyTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
@@ -32,44 +42,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TwittyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                   Navigation()
-                }
+                DestinationsNavHost(navGraph = NavGraphs.root)
             }
         }
     }
 }
 
 
+@RootNavGraph(start = true )
+@Destination
 @Composable
-fun Navigation(){
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "splash_screen"){
-        composable("splash_screen"){
-            SplashScreen(navController = navController)
-        }
-
-        // Main Screen
-        composable("main_screen"){
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-                Text(text = "Main Screen", color = Color.Black, fontSize = 24.sp)
-            }
-        }
-    }
-}
-
-
-@Composable
-fun SplashScreen(navController: NavController){
+fun SplashScreen(
+    navigator: DestinationsNavigator
+){
     val scale = remember{
         androidx.compose.animation.core.Animatable(0f)
     }
 
-    //AnimationEffect
     LaunchedEffect(key1 = true){
         scale.animateTo(
             targetValue =  0.7f,
@@ -79,14 +68,29 @@ fun SplashScreen(navController: NavController){
                     OvershootInterpolator(4f).getInterpolation(it)
                 })
         )
-        delay(400L)
-        navController.navigate("main_screen")
+        delay(1000L)
+        navigator.navigate(HomeScreenDestination.route)
     }
     //Image
     Box(contentAlignment =  Alignment.Center, modifier = Modifier.fillMaxSize()){
         Image(painter = painterResource(id = R.drawable.twitterlogo),
             contentDescription = "Logo",
             modifier = Modifier.scale(scale.value))
+
+    }
+}
+
+@Destination
+@Composable
+fun HomeScreen(
+    navigator: DestinationsNavigator
+){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Profile Screen: MainScreen", textAlign = TextAlign.Center)
 
     }
 }
